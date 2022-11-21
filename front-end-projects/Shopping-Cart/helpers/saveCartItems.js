d2e65d48c3e876c6d2b83b293cf75db9f731e64f
Danilo Couto <88905222+Danilo-Couto@users.noreply.github.com>
2022-11-21 19:28:13 -0300

    Delete front-end-projects directory

--- front-end-projects/Shopping-Cart/helpers/saveCartItems.js
+++
@@ -1,7 +0,0 @@
-// salvar os itens do carrinho de compras no localStorage, em uma chave denominada cartItems. Todas as adições e remoções devem ser abordadas para que a lista esteja sempre atualizada.
-
-const saveCartItems = (item) => localStorage.setItem('cartItems', item);
-  
-if (typeof module !== 'undefined') {
-  module.exports = saveCartItems;
-}
